07ce23e3487c5be081d2b4af4690bd7f930f3b59
taekwondodev <taekwondodev@outlook.it>
2024-05-17 16:07:06 +0200

    Initial Commit

---
+++ Deaf/DeafApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeafApp.swift
+//  Deaf
+//
+//  Created by Davide Galdiero on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DeafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
